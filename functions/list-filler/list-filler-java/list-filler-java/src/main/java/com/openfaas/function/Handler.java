502359e67337e443867b646793093062b110b329
dfquaresma <davidferreiraquaresma@gmail.com>
2018-12-18 17:47:39 -0300

    Set the final logic of list-filler-java.

--- functions/list-filler/list-filler-java/list-filler-java/src/main/java/com/openfaas/function/Handler.java
+++ functions/list-filler/list-filler-java/list-filler-java/src/main/java/com/openfaas/function/Handler.java
@@ -15,47 +15,17 @@ import java.util.ArrayList;
 public class Handler implements com.openfaas.model.IHandler {
 
     public IResponse Handle(IRequest req) {
-	List<GarbageCollectorMXBean> gcs = ManagementFactory.getGarbageCollectorMXBeans();
-	GarbageCollectorMXBean scavenge = gcs.get(0);
-	GarbageCollectorMXBean markSweep = gcs.get(1);
-
-	long countBeforeScavenge = scavenge.getCollectionCount();
-	long timeBeforeScavenge = scavenge.getCollectionTime();
-        long countBeforeMarkSweep = markSweep.getCollectionCount();
-        long timeBeforeMarkSweep = markSweep.getCollectionTime();
-
 	long before = System.currentTimeMillis();
-	businessLogic();
-        long after = System.currentTimeMillis();
-
-        long countAfterScavenge = scavenge.getCollectionCount();
-        long timeAfterScavenge = scavenge.getCollectionTime();
-        long countAfterMarkSweep = markSweep.getCollectionCount();
-        long timeAfterMarkSweep = markSweep.getCollectionTime();
-
-
-        String processName = java.lang.management.ManagementFactory.getRuntimeMXBean().getName();
-        long pid = Long.parseLong(processName.split("@")[0]);
-
-        String data =
-                "Business Logic Time in Milliseconds: " + Long.toString(after - before) +
-                ",  pid: " + Long.toString(pid) + System.lineSeparator() +
-                "Scavenge Number of Collections: " + Long.toString(countAfterScavenge - countBeforeScavenge) +
-                ", Scavenge Collections Time Spent in Milliseconds: " + Long.toString(timeAfterScavenge - timeBeforeScavenge) + System.lineSeparator() +
-                "MarkSweep Number of Collections: " + Long.toString(countAfterMarkSweep - countBeforeMarkSweep) +
-                ", MarkSweep Collections Time Spent in Milliseconds: " + Long.toString(timeAfterMarkSweep - timeBeforeMarkSweep);
-
-	Response res = new Response();
- 	res.setBody(data);
-	return res;
-    }
-
-    public void businessLogic() {
         int size = (int) Math.pow(2, 21);
         List<Integer> list  = new ArrayList();
         for (int i = 0; i < size; i++) {
                 list.add(i);
         }
+        long after = System.currentTimeMillis();
+
+	Response res = new Response();
+ 	res.setBody(Long.toString(after - before));
+	return res;
     }
 
 }
