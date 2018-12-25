3d322c63e3045ba5aa4fb37555b55cc3b6c04393
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2018-12-25 00:33:56 -0300

    Add initial implementations of thumbnailator function.

--- functions/thumbnailator/thumbnailator/src/main/java/com/openfaas/function/Handler.java
+++ functions/thumbnailator/thumbnailator/src/main/java/com/openfaas/function/Handler.java
@@ -5,13 +5,55 @@ import com.openfaas.model.IResponse;
 import com.openfaas.model.IRequest;
 import com.openfaas.model.Response;
 
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+
+import java.util.List;
+import java.util.ArrayList;
+
 import net.coobird.thumbnailator.Thumbnails;
 
 public class Handler implements com.openfaas.model.IHandler {
 
     public IResponse Handle(IRequest req) {
-        Response res = new Response();
-	res.setBody(req.getBody());
-        return res;
+	List<GarbageCollectorMXBean> gcs = ManagementFactory.getGarbageCollectorMXBeans();
+	GarbageCollectorMXBean scavenge = gcs.get(0);
+	GarbageCollectorMXBean markSweep = gcs.get(1);
+
+	long countBeforeScavenge = scavenge.getCollectionCount();
+	long timeBeforeScavenge = scavenge.getCollectionTime();
+        long countBeforeMarkSweep = markSweep.getCollectionCount();
+        long timeBeforeMarkSweep = markSweep.getCollectionTime();
+
+	long before = System.currentTimeMillis();
+        callThumbnailFunction();
+	long after = System.currentTimeMillis();
+
+        long countAfterScavenge = scavenge.getCollectionCount();
+        long timeAfterScavenge = scavenge.getCollectionTime();
+        long countAfterMarkSweep = markSweep.getCollectionCount();
+        long timeAfterMarkSweep = markSweep.getCollectionTime();
+
+        String processName = java.lang.management.ManagementFactory.getRuntimeMXBean().getName();
+        long pid = Long.parseLong(processName.split("@")[0]);
+
+        String data =
+		Long.toString(pid) + "," + // Pid
+                Long.toString(after - before) + "," + // Business Logic Time in Milliseconds
+                Long.toString(countAfterScavenge - countBeforeScavenge) + "," + // Scavenge Number of Collections
+                Long.toString(timeAfterScavenge - timeBeforeScavenge) + "," + // Scavenge Collections Time Spent in Milliseconds
+                Long.toString(countAfterMarkSweep - countBeforeMarkSweep) + "," + // MarkSweep Number of Collections
+                Long.toString(timeAfterMarkSweep - timeBeforeMarkSweep); // MarkSweep Collections Time Spent in Milliseconds
+
+	Response res = new Response();
+ 	res.setBody(data);
+	return res;
+
     }
+
+    public void callThumbnailFunction() {
+	Thumbnails.of("image.jpg")
+        .size(160, 160);
+    }
+
 }
