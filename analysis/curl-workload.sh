1116f9283eea7adf12f955c2e91456462586fb2c
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2019-01-17 16:40:33 -0300

    Update curl workload script.

--- analysis/curl-workload.sh
+++ analysis/curl-workload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 date
 set -x
-echo -e "pid,business_time(ms),scavenge_count,scavenge_time,marksweep_count,marksweep_time" > ${FILE_NAME}
+echo -e "pid,execution_time(ms),scavenge_count,scavenge_time,marksweep_count,marksweep_time" > ${FILE_NAME}
 for i in `seq 1 ${NUMBER_OF_REQUESTS}`
 do
 	curl -X GET ${URL} >> ${FILE_NAME}
