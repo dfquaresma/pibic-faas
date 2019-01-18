df362546ea8713afc9489f36acd0525a92b42f14
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2019-01-18 08:23:13 -0300

    Update quantiles visualization.

--- analysis/functions.R
+++ analysis/functions.R
@@ -17,31 +17,6 @@ interval = function(data, p0, p1, p2, names) {
   return(data)
 }
 
-meanVarSd = function(nocollect, withcollect) {
-  
-  noCollect = c(mean(nocollect$execution_time),
-                var(nocollect$execution_time),
-                sd(nocollect$execution_time))
-  
-  withCollect = c(mean(withcollect$execution_time),
-                  var(withcollect$execution_time),
-                  sd(withcollect$execution_time))
-  
-  data.frame(
-    statistic = c("mean", "var", "sd"), 
-    noCollect = noCollect, 
-    withCollect = withCollect,
-    comparison = withCollect / noCollect)
-}
-
-quantile_wrapped = function(data) {
-  quantile(data, c(.0, .25, .50, .75, .90, .95, .99, .999, .9999, .99999, 1))
-}
-
-quantile_wrapped_for_execution_time = function(data) {
-  quantile_wrapped(data$execution_time)
-}
-
 plot_ecdf = function(nocollect, withcollect) {
   ecdf_nocollect = ecdf(nocollect$execution_time)
   ecdf_withcollect = ecdf(withcollect$execution_time)
@@ -73,3 +48,57 @@ build_barplot = function(results) {
           xlab="tempo de execução", col=c("white","gray", "black"),
           names = c("6 ms", "6-7 ms", "7-14 ms", "14-181 ms"), legend = rownames(counts))
 }
+
+meanVarSd = function(nocollect, withcollect) {
+  
+  noCollect = c(mean(nocollect$execution_time),
+                var(nocollect$execution_time),
+                sd(nocollect$execution_time))
+  
+  withCollect = c(mean(withcollect$execution_time),
+                  var(withcollect$execution_time),
+                  sd(withcollect$execution_time))
+  
+  data.frame(
+    statistic = c("mean", "var", "sd"), 
+    noCollect = noCollect, 
+    withCollect = withCollect,
+    comparison = withCollect / noCollect)
+}
+
+coletas_dataframe = function(results) {
+  data.frame(
+     tipo = c("scavenge (young gen)", 
+                      "marksweep (old gen)"),
+     coletas = c(sum(results$scavenge_count), 
+                sum(results$marksweep_count))
+     )
+}
+
+cor_dataframe = function(results) {
+  data.frame(
+    correlacao = c("Tempo de serviço X número de coletas", 
+                   "Tempo de serviço X tempo coletando"), 
+    valor = c(cor(results$execution_time, results$scavenge_count + results$marksweep_count), 
+              cor(results$execution_time, time_collecting))
+    )
+}
+
+quantile_wrapped = function(data) {
+  quantile(data, c(.0, .25, .50, .75, .90, .95, .99, .999, .9999, .99999, 1))
+}
+
+quantile_wrapped_for_execution_time = function(data) {
+  quantile_wrapped(data$execution_time)
+}
+
+quantiles_dataframe_comparison = function(nocollect, withcollect) {
+  comparison = (quantile_wrapped_for_execution_time(withcollect) 
+                / quantile_wrapped_for_execution_time(nocollect))
+  data.frame(
+    nocollect = quantile_wrapped_for_execution_time(nocollect),
+    withcollect = quantile_wrapped_for_execution_time(withcollect),
+    comparison = comparison
+  )
+}
+
