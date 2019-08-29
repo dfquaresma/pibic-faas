112b8a33aa80714664f221f15b0e0c4208ca333e
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-08-29 20:56:43 -0300

    Add listfiller and thumbnailator containers version

---
+++ containers/thumbnailator/function/src/main/java/com/openfaas/function/Handler.java
@@ -0,0 +1,99 @@
+package com.openfaas.function;
+
+import com.openfaas.model.IHandler;
+import com.openfaas.model.IResponse;
+import com.openfaas.model.IRequest;
+import com.openfaas.model.Response;
+
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.Error;
+
+import net.coobird.thumbnailator.Thumbnails;
+import java.net.URL;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+public class Handler implements com.openfaas.model.IHandler {
+
+    public IResponse Handle(IRequest req) {
+        List<GarbageCollectorMXBean> gcs = ManagementFactory.getGarbageCollectorMXBeans();
+        GarbageCollectorMXBean scavenge = gcs.get(0);
+        GarbageCollectorMXBean markSweep = gcs.get(1);
+
+        long countBeforeScavenge = scavenge.getCollectionCount();
+        long timeBeforeScavenge = scavenge.getCollectionTime();
+        long countBeforeMarkSweep = markSweep.getCollectionCount();
+        long timeBeforeMarkSweep = markSweep.getCollectionTime();
+        long before = System.currentTimeMillis();
+
+        String err = callFunction();
+
+        long after = System.currentTimeMillis();
+        long countAfterScavenge = scavenge.getCollectionCount();
+        long timeAfterScavenge = scavenge.getCollectionTime();
+        long countAfterMarkSweep = markSweep.getCollectionCount();
+        long timeAfterMarkSweep = markSweep.getCollectionTime();
+
+        String processName = java.lang.management.ManagementFactory.getRuntimeMXBean().getName();
+        long pid = Long.parseLong(processName.split("@")[0]);
+
+        String output = err + System.lineSeparator();
+        if (err.length() == 0) {
+            output = Long.toString(pid) + "," + // Pid
+                Long.toString(after - before) + "," + // Business Logic Time in Milliseconds
+                Long.toString(countAfterScavenge - countBeforeScavenge) + "," + // Scavenge Number of Collections
+                Long.toString(timeAfterScavenge - timeBeforeScavenge) + "," + // Scavenge Collections Time Spent in Milliseconds
+                Long.toString(countAfterMarkSweep - countBeforeMarkSweep) + "," + // MarkSweep Number of Collections
+                Long.toString(timeAfterMarkSweep - timeBeforeMarkSweep); // MarkSweep Collections Time Spent in Milliseconds
+        }
+
+        Response res = new Response();
+        res.setBody(output);
+        return res;
+    }
+
+    static int rotate;
+    static double outputQuality, scale;
+    static BufferedImage image;
+    static {
+        try{
+            rotate = Integer.parseInt(System.getenv("rotate"));
+            outputQuality = Double.parseDouble(System.getenv("output_quality"));
+            scale = Double.parseDouble(System.getenv("scale"));
+            image = ImageIO.read(new URL(System.getenv("image_url")));
+
+        } catch(Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public String callFunction() {
+        String err = "";
+        try {
+            Thumbnails.of(image)
+                .scale(scale)
+                .rotate(rotate)
+                .outputQuality(outputQuality)
+                .asBufferedImage();
+            
+        } catch (Exception e) {
+            err = e.toString() + System.lineSeparator()
+            		+ e.getCause() + System.lineSeparator()
+            		+ e.getMessage();
+            e.printStackTrace();
+
+        } catch (Error e) {
+            err = e.toString() + System.lineSeparator()
+            		+ e.getCause() + System.lineSeparator()
+            		+ e.getMessage();
+            e.printStackTrace();
+        }
+
+        return err;
+    }
+
+}
