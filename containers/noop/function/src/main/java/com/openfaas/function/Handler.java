b8de986c9434b8a4eefcc5360639403535241c27
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-08-29 21:17:41 -0300

    Add noop func container

---
+++ containers/noop/function/src/main/java/com/openfaas/function/Handler.java
@@ -0,0 +1,59 @@
+package com.openfaas.function;
+
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+
+import java.util.List;
+
+import com.openfaas.model.IHandler;
+import com.openfaas.model.IRequest;
+import com.openfaas.model.IResponse;
+import com.openfaas.model.Response;
+
+import java.util.ArrayList;
+import java.lang.Error;
+
+public class Handler implements com.openfaas.model.IHandler {
+
+    public IResponse Handle(IRequest req) {
+        List<GarbageCollectorMXBean> gcs = ManagementFactory.getGarbageCollectorMXBeans();
+        GarbageCollectorMXBean scavenge = gcs.get(0);
+        GarbageCollectorMXBean markSweep = gcs.get(1);
+
+        long countBeforeScavenge = scavenge.getCollectionCount();
+        long timeBeforeScavenge = scavenge.getCollectionTime();
+        long countBeforeMarkSweep = markSweep.getCollectionCount();
+        long timeBeforeMarkSweep = markSweep.getCollectionTime();
+        long before = System.currentTimeMillis();
+
+        String err = callFunction();
+
+        long after = System.currentTimeMillis();
+        long countAfterScavenge = scavenge.getCollectionCount();
+        long timeAfterScavenge = scavenge.getCollectionTime();
+        long countAfterMarkSweep = markSweep.getCollectionCount();
+        long timeAfterMarkSweep = markSweep.getCollectionTime();
+
+        String processName = java.lang.management.ManagementFactory.getRuntimeMXBean().getName();
+        long pid = Long.parseLong(processName.split("@")[0]);
+
+        String output = err + System.lineSeparator();
+        if (err.length() == 0) {
+            output = Long.toString(pid) + "," + // Pid
+                Long.toString(after - before) + "," + // Business Logic Time in Milliseconds
+                Long.toString(countAfterScavenge - countBeforeScavenge) + "," + // Scavenge Number of Collections
+                Long.toString(timeAfterScavenge - timeBeforeScavenge) + "," + // Scavenge Collections Time Spent in Milliseconds
+                Long.toString(countAfterMarkSweep - countBeforeMarkSweep) + "," + // MarkSweep Number of Collections
+                Long.toString(timeAfterMarkSweep - timeBeforeMarkSweep); // MarkSweep Collections Time Spent in Milliseconds
+        }
+
+        Response res = new Response();
+        res.setBody(output);
+        return res;
+    }
+
+    public String callFunction() {
+        return null;
+    }
+
+}
