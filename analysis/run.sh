9129cb81fc2c0aec4f21b4ddb433be136db503b7
Ubuntu <ubuntu@dfq-simulation.lsd.ufcg.edu.br>
2019-08-31 21:24:58 +0000

    Add run script and update thumbnailator image on dockerfile

---
+++ analysis/run.sh
@@ -0,0 +1,21 @@
+
+#!/bin/bash
+date
+set -x
+
+echo "EXPID_START: ${EXPID_START:=0}"
+echo "EXPID_END: ${EXPID_END:=2}"
+echo "FUNCTIONS: ${FUNCTIONS:=thumb listfiller}"
+
+for func in ${FUNCTIONS};
+do
+    sudo rm -rf "./results/${func}-containter-*.csv"
+    for expid in `seq ${EXPID_START} ${EXPID_END}`
+    do
+        sudo docker rm -f ${func};
+        sudo docker run -d --network=host --cpus=1.0 --cpuset-cpus=0 --rm --name ${func} ${func};
+        sleep 5
+        NUMBER_OF_REQUESTS=10000 URL="localhost:8081" FILE_NAME="./results/${func}-containter-${expid}.csv" bash curl-workload.sh;
+    done
+done
+
