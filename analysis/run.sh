21059e5dac7835b67e25cb2ccfee2862c015852e
Ubuntu <ubuntu@dfq-simulation.lsd.ufcg.edu.br>
2019-08-31 23:43:07 +0000

    Update run.sh output format

--- analysis/run.sh
+++ analysis/run.sh
@@ -5,17 +5,17 @@ set -x
 
 echo "EXPID_START: ${EXPID_START:=0}"
 echo "EXPID_END: ${EXPID_END:=2}"
-echo "FUNCTIONS: ${FUNCTIONS:=thumb listfiller}"
+echo "FUNCTION: ${FUNCTIONS:=thumb}"
 
-for func in ${FUNCTIONS};
+for func in ${FUNCTION};
 do
-    sudo rm -rf "./results/${func}-containter-*.csv"
+    sudo rm -rf "./results/${func}-container-*.csv"
     for expid in `seq ${EXPID_START} ${EXPID_END}`
     do
         sudo docker rm -f ${func};
         sudo docker run -d --network=host --cpus=1.0 --cpuset-cpus=0 --rm --name ${func} ${func};
         sleep 5
-        NUMBER_OF_REQUESTS=10000 URL="localhost:8081" FILE_NAME="./results/${func}-containter-${expid}.csv" bash curl-workload.sh;
+        NUMBER_OF_REQUESTS=10000 URL="localhost:8081" FILE_NAME="./results/${func}-container-${expid}.csv" bash curl-workload.sh;
     done
 done
 
